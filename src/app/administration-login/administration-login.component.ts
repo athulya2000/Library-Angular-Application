ecdaa22810e22fedf7cbba7d381783294331cb46
athulya2000 <athulyav00@gmail.com>
2022-11-24 19:48:11 +0530

    index completed

---
+++ src/app/administration-login/administration-login.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-administration-login',
+  templateUrl: './administration-login.component.html',
+  styleUrls: ['./administration-login.component.css']
+})
+export class AdministrationLoginComponent {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { AdministrationLoginComponent } from './administration-login/administration-login.component';
+import { RouterModule, Routes } from '@angular/router';
+const myRoute:Routes=[
+  {
+    path:"",
+    component:AdministrationLoginComponent
+  }
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdministrationLoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
