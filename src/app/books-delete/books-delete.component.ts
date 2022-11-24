911b3238522366f0458b76c4e60d9c5c7116b19d
athulya2000 <athulyav00@gmail.com>
2022-11-24 21:02:52 +0530

    delete completed

---
+++ src/app/books-delete/books-delete.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-books-delete',
+  templateUrl: './books-delete.component.html',
+  styleUrls: ['./books-delete.component.css']
+})
+export class BooksDeleteComponent {
+
+}
