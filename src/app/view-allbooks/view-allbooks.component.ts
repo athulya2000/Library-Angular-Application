5290363eb92d114e77b6ba900adb5d5b11c77371
athulya2000 <athulyav00@gmail.com>
2022-11-25 11:37:33 +0530

    commited

--- src/app/view-allbooks/view-allbooks.component.ts
+++ src/app/view-allbooks/view-allbooks.component.ts
@@ -6,5 +6,75 @@ import { Component } from '@angular/core';
   styleUrls: ['./view-allbooks.component.css']
 })
 export class ViewAllbooksComponent {
-
+books:any=[
+  {
+    "titie":"Wings Of Fire",
+    "image":"https://3.imimg.com/data3/VM/WT/MY-12313319/kalam-500x500.jpeg",
+    "author":"APJ Abdulkalam",
+    "price":"250"
+  },
+  {
+    
+    "titie":"Born To Write",
+    "image":"https://m.media-amazon.com/images/I/5110piHSh9L._SX346_BO1,204,203,200_.jpg",
+    "author":"Charis Cotter",
+    "price":"200"
+  },
+  {
+    
+    "titie":"The Alchemist",
+    "image":"https://m.media-amazon.com/images/I/915hExGyhVL.jpg",
+    "author":"Paulo Coelho",
+    "price":"150"
+  },
+  {
+    
+    "titie":"Harry Potter",
+    "image":"https://www.famousauthors.org/images/childrens-books/harry-potter-and-the-philosophers-stone.jpg",
+    "author":"RK Rowling",
+    "price":"256"
+  },
+  {
+    
+    "titie":">Kite Runner",
+    "image":"https://cdn.lifehack.org/wp-content/uploads/2022/11/51vRNqL61aL._SL500_.webp",
+    "author":"Khaled Hosseini",
+    "price":"300"
+  },
+  {
+    
+    "titie":"I Have A Dream",
+    "image":"https://m.media-amazon.com/images/I/71bJLYaVAsL.jpg",
+    "author":"I Have A Dream",
+    "price":"250"
+  },
+  {
+    
+    "titie":"Atomic Habits",
+    "image":"https://jamesclear.com/wp-content/uploads/2022/09/9M-atomic-habits-dots.png",
+    "author":"James Clear",
+    "price":"300"
+  },
+  {
+    
+    "titie":"The Guide",
+    "image":"https://www.frontlist.in/storage/uploads/2021/02/The-Guide-by-RK-Narayan.jpg",
+    "author":"RK Narayanan",
+    "price":"220"
+  },
+  {
+    
+    "titie":"The Ring Of Bright Water",
+    "image":"https://fivebooks.com/app/uploads/2017/04/1910065099.01.LZ_.jpg",
+    "author":"Gavin Maxwell",
+    "price":"250"
+  },
+  {
+    
+    "titie":">Deadland",
+    "image":"https://www.bookgeeks.in/wp-content/uploads/2018/01/Alice-in-Deadland-Trilogy-by-Mainak-Dhar.jpg",
+    "author":"Mainack Dhar",
+    "price":"250"
+  }
+]
 }
